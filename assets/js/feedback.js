46a4d6031929b107e9772cb4c8595d47593e27da
Bulat <bulatkazan@yahoo.com>
2023-02-15 15:25:33 +0600

    feat: hid about section
    feat: add new menu item and page, add dropdown
    feat: add identifier field to menus in config.toml
    feat: menus names, add template md files
    feat: add language support to about dropdown menu
    feat: change about layout
    feat: added grid mixin
    feat: alternative css for sections to redesign
    fix: removed extra styles
    feat: renamed files
    feat: cards and designs import
    feat: added faq cards
    fix: main restyle added
    feat: controllable card
    feat: about section bottom navigation
    delete test text
    feat: about baseof redesign
    feat: sections redesign
    feat: moved footer navigation to shortcodes
    feat: added feedback shortcode
    feat: added analytics script
    update content of falco.md, add img
    feat: add shortcode for 2 columns with image
    feat: change structure, delete shortcodes from md
    feat: get data from shortcode
    feat: add upper card
    feat: add btn, delete aliases, add test content
    fix: styles
    fix: faq section order
    update content of use-cases
    fix: card text tag changed to div
    feat: add cards to falco.md
    feat: fixed about section home page styles
    feat: faq page restyle
    feat: buttons redesign
    feat: btn link redesign
    fix: removed unused shortcodes
    change two_column_block_img.html shortcode to show correct layout on mobile, change grid for cards
    feat: added faq section content
    change from png to svg, add lazy loading
    feat: add lazy loading default
    feat: card-sm class added
    feat: tabpane v1
    feat: tabpane v2
    feat: updated content of about and faq sections
    fix: updated content, renamed classname to class in card
    fix: removed unused files
    
    Signed-off-by: veralex <alexey.verevkin@akvelon.com>

---
+++ assets/js/feedback.js
@@ -0,0 +1,40 @@
+(() => {
+  const [yesButton] = document.getElementsByClassName("feedback--answer-yes");
+  const [noButton] = document.getElementsByClassName("feedback--answer-no");
+  const [yesResponse] = document.getElementsByClassName("feedback--response-yes");
+  const [noResponse] = document.getElementsByClassName("feedback--response-no");
+  
+  const disableButtons = () => {
+    yesButton.disabled = true;
+    noButton.disabled = true;
+  };
+  const sendFeedback = (value) => {
+    if (typeof ga !== "function") return;
+    const args = {
+      command: "send",
+      hitType: "event",
+      category: "Helpful",
+      action: "click",
+      label: window.location.pathname,
+      value: value,
+    };
+    ga(
+      args.command,
+      args.hitType,
+      args.category,
+      args.action,
+      args.label,
+      args.value
+    );
+  };
+  yesButton.addEventListener("click", () => {
+    yesResponse.classList.remove("d-none");
+    disableButtons();
+    sendFeedback(1);
+  });
+  noButton.addEventListener("click", () => {
+    noResponse.classList.remove("d-none");
+    disableButtons();
+    sendFeedback(0);
+  });
+})();
---
+++ content/en/about/use-cases.md
@@ -0,0 +1,16 @@
+---
+title: Use case page
+---
+
+{{< blocks/content wrap="col" >}}
+## What you can do with Falco today
+{{< /blocks/content >}}
+
+{{< blocks/content content="html" wrap="col">}}
+{{< footer_nav 
+  prev="/about/why-falco"
+  prevTitle="Why Falco?"
+  next="/about/mitre" 
+  nextTitle="MITRE ATT&CK Framework" 
+>}}
+{{< /blocks/content >}}
