48a9041913b6830fc21ed796ef29d97885e19ccf
veralex <alexey.verevkin@akvelon.com>
2023-03-27 23:52:36 +0300

    fix: merge conflict
    Signed-off-by: veralex <alexey.verevkin@akvelon.com>

---
+++ assets/js/subscribe-form.js
@@ -0,0 +1,21 @@
+(() => {
+  const form = document.getElementById("events-subscribe");
+  const submitButton = form.querySelector("button[type=submit]");
+
+  const checkboxes = Array.from(form.querySelectorAll("input[type=checkbox]"));
+  const nameInput = form.querySelector("[id=subscribeName]");
+  const emailInput = form.querySelector("[id=subscribeEmail]");
+
+  form.addEventListener("change", () => {
+    submitButton.disabled = [
+      ...checkboxes.map(({ checked }) => checked),
+      !!nameInput.value,
+      !!emailInput.value,
+    ].some((val) => !val);
+  });
+
+  form.addEventListener('submit', e => {
+    // TODO: handler
+    e.preventDefault();
+  })
+})();
