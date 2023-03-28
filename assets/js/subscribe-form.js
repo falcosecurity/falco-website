7864c178221453d79b5e35e158313c50b11a3816
veralex <alexey.verevkin@akvelon.com>
2023-03-28 23:15:38 +0300

    fix: landing page minor issues
    Signed-off-by: veralex <alexey.verevkin@akvelon.com>

--- assets/js/subscribe-form.js
+++ assets/js/subscribe-form.js
@@ -1,21 +1,26 @@
 (() => {
   const form = document.getElementById("events-subscribe");
-  const submitButton = form.querySelector("button[type=submit]");
+  
+  if (form) {
+    const submitButton = form.querySelector("button[type=submit]");
 
-  const checkboxes = Array.from(form.querySelectorAll("input[type=checkbox]"));
-  const nameInput = form.querySelector("[id=subscribeName]");
-  const emailInput = form.querySelector("[id=subscribeEmail]");
+    const checkboxes = Array.from(
+      form.querySelectorAll("input[type=checkbox]")
+    );
+    const nameInput = form.querySelector("[id=subscribeName]");
+    const emailInput = form.querySelector("[id=subscribeEmail]");
 
-  form.addEventListener("change", () => {
-    submitButton.disabled = [
-      ...checkboxes.map(({ checked }) => checked),
-      !!nameInput.value,
-      !!emailInput.value,
-    ].some((val) => !val);
-  });
+    form.addEventListener("change", () => {
+      submitButton.disabled = [
+        ...checkboxes.map(({ checked }) => checked),
+        !!nameInput.value,
+        !!emailInput.value,
+      ].some((val) => !val);
+    });
 
-  form.addEventListener('submit', e => {
-    // TODO: handler
-    e.preventDefault();
-  })
+    form.addEventListener("submit", (e) => {
+      // TODO: handler
+      e.preventDefault();
+    });
+  }
 })();
