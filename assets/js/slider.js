d9d817d82df3be0b4cf65d3c4c1ef1a561c017fc
veralex <alexey.verevkin@akvelon.com>
2023-04-10 17:22:57 +0300

    fix: blog accessibility and performance
    Signed-off-by: veralex <alexey.verevkin@akvelon.com>

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,27 +1,29 @@
-const swiper = new Swiper('.swiper', {
-  loop: false,
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  pagination: {
-    el: ".swiper__pagination",
-    clickable: true,
-    bulletClass: "swiper__bullet",
-    bulletActiveClass: "swiper__bullet--active",
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
+const initSwiper = () =>
+  {
+    console.log('init')
+    new Swiper(".swiper", {
+    loop: false,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    pagination: {
+      el: ".swiper__pagination",
+      clickable: true,
+      bulletClass: "swiper__bullet",
+      bulletActiveClass: "swiper__bullet--active",
     },
-    768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    }
-
-  },
-  navigation: {
-    nextEl: ".swiper__control-reverse",
-    prevEl: ".swiper__control",
-    disabledClass: "swiper__control--disabled"
-  }
-});
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper__control-reverse",
+      prevEl: ".swiper__control",
+      disabledClass: "swiper__control--disabled",
+    },
+  });}
