2a43f71f530cf032134c1a8e446653f62ac4a766
veralex <alexey.verevkin@akvelon.com>
2023-04-15 13:28:41 +0300

    fix: added faq card copy link button
    Signed-off-by: veralex <alexey.verevkin@akvelon.com>

--- assets/js/copy-link.js
+++ assets/js/copy-link.js
@@ -1,10 +1,24 @@
 const copyLinkClickHandler = (link) => {
   navigator.clipboard.writeText(link);
 
-  const toast = $('#toast');
-  const [el] = toast;
-  el.classList.remove('d-none');
-  el.getElementsByClassName('toast__content')[0].innerText = 'Link copied to clipboard';
-  
-  toast.toast('show');
-}
+  const toast = $("#toast");
+
+  if (toast.length) {
+    const [el] = toast;
+    el.classList.remove("d-none");
+    el.getElementsByClassName("toast__content")[0].innerText =
+      "Link copied to clipboard";
+
+    toast.toast("show");
+  }
+};
+
+document
+  .querySelectorAll("[data-faq-id]")
+  .forEach((el) =>
+    el.addEventListener("click", (e) =>
+      copyLinkClickHandler(
+        `${window.location.origin}/about/faq/#${el.getAttribute("data-faq-id")}`
+      )
+    )
+  );
